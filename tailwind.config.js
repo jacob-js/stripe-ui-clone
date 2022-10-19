882aee346c083c80ada291b052d60000a74abf1a
Jacob Dev <mercihabam@gmail.com>
2022-10-19 19:55:53 +0200

    add more sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['*.html'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        '4xl': '36px'
+      },
+      lineHeight: {
+        '1.5': 1.5
+      },
+      boxShadow: {
+        'el': '0 50px 100px -20px rgb(50 50 93 / 25%), 0 30px 60px -30px rgb(0 0 0 / 30%), inset 0 -2px 6px 0 rgb(10 37 64 / 35%)'
+      }
+    },
     clipPath: {
       mypg: 'polygon(0 0, 100% 0, 100% 50%, 0% 100%)'
     }
