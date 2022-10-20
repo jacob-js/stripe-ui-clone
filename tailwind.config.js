db951065bf88c9bfe8c50f7e98d345e54f1a858d
Jacob Dev <mercihabam@gmail.com>
2022-10-20 15:06:11 +0200

    add planet section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,28 @@ module.exports = {
       },
       boxShadow: {
         'el': '0 50px 100px -20px rgb(50 50 93 / 25%), 0 30px 60px -30px rgb(0 0 0 / 30%), inset 0 -2px 6px 0 rgb(10 37 64 / 35%)'
+      },
+      colors: {
+        'dark': '#0a2540',
+        bright: '#adbdcc',
+        secondary: '#00d4ff'
+      },
+      fontSize: {
+        '4.1xl': '38px',
+        '1sm': 'calc(1rem - 1px)'
+      },
+      width: {
+        '9/10': '90%'
+      },
+      height: {
+        '1.5screen': '150vh',
+        '1.2screen': '120vh',
+        '1.1screen': '110vh',
       }
     },
     clipPath: {
-      mypg: 'polygon(0 0, 100% 0, 100% 50%, 0% 100%)'
+      mypg: 'polygon(0 0, 100% 0, 100% 50%, 0% 100%)',
+      half:"polygon(0 17%, 100% 2%, 100% 100%, 0% 100%)"
     }
   },
   plugins: [
